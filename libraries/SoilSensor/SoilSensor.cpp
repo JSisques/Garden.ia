923c2857b0339aa9e192b765e89198a65ccd9982
Javi Plaza <javierplazasisques@gmail.com>
2021-06-12 20:54:03 +0200

    Soil Sensor library

---
+++ libraries/SoilSensor/SoilSensor.cpp
@@ -0,0 +1,49 @@
+#include "Arduino.h"
+#include "SoilSensor.h"
+
+SoilSensor::SoilSensor(int inputPin, int inputMaxValue, int inputMinValue){
+    pin = inputPin;
+    maxValue = inputMaxValue;
+    minValue = inputMinValue;
+}
+
+void SoilSensor::setPin(int inputPin){
+    pin = inputPin;
+}
+
+void SoilSensor::setHumidity(int inputHumidity){
+    humidity = inputHumidity;
+}
+
+void SoilSensor::setMaxValue(int inputMaxValue){
+    maxValue = inputMaxValue;
+}
+
+void SoilSensor::setMinValue(int inputMinValue){
+    minValue = inputMinValue;
+}
+
+int SoilSensor::getPin(){
+    return pin;
+}
+
+int SoilSensor::getHumidity(){
+    return humidity;
+}
+
+int SoilSensor::getMaxValue(){
+    return maxValue;
+}
+
+int SoilSensor::getMinValue(){
+    return minValue;
+}
+
+int SoilSensor::checkHumidity(){
+    humidity = analogRead(pin);
+
+    humidity = humidity * 100 / maxValue;
+
+    return humidity;
+
+}
---
+++ libraries/SoilSensor/SoilSensor.h
@@ -0,0 +1,30 @@
+#ifndef SoilSensor_h
+#define SoilSensor_h
+
+#include "Arduino.h"
+
+class SoilSensor{
+
+    private:
+        int pin;
+        int humidity;
+        int maxValue;
+        int minValue;
+
+    public:
+        SoilSensor(int pin, int maxValue, int minValue);
+
+        void setPin(int pin);
+        void setHumidity(int humidity);
+        void setMaxValue(int maxValue);
+        void setMinValue(int minValue);
+
+        int getPin();
+        int getHumidity();
+        int getMaxValue();
+        int getMinValue();
+
+        int checkHumidity();
+    };
+
+#endif
