68de4df443230c5f0f95da9e0b686759784e4079
Javi Plaza <javierplazasisques@gmail.com>
2021-06-14 17:54:58 +0200

    Created Data library

--- Gardenia.ino
+++ Gardenia.ino
@@ -2,16 +2,21 @@
 #include <WaterLevel.h>
 #include <LightSensor.h>
 
+LightSensor ls(9);
 PowerSave ps;
 
 void setup() {
-
 }
 
 void loop() {
 
   //Check light
 
+  //Check soil humidity
+  //Check water level
+  //Water (optional)
+
   //Sleep 1 hour
+  ps.sleep(3600);
 
 }
