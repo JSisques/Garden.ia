2fbcdcc8cb0cf63258ee7a457029da430c865808
Javi Plaza <javierplazasisques@gmail.com>
2021-06-15 17:44:05 +0200

    Updated Temperature Sensor library

--- libraries/TemperatureSensor/TemperatureSensor.cpp
+++ libraries/TemperatureSensor/TemperatureSensor.cpp
@@ -1,5 +1,84 @@
 #include "Arduino.h"
 #include "TemperatureSensor.h"
+#include "DHT.h"
 
 TemperatureSensor::TemperatureSensor(){
+
 }
+
+TemperatureSensor::TemperatureSensor(String inputName, float inputPin){
+    name = inputName;
+    pin = inputPin;
+
+    dht = DHT(pin, DHT11);
+}
+
+void TemperatureSensor::setDht(DHT inputDht){
+    dht = inputDht;
+}
+
+void TemperatureSensor::setName(String inputName){
+    name = inputName;
+}
+
+void TemperatureSensor::setPin(float inputPin){
+    pin = inputPin;
+}
+
+void TemperatureSensor::setTemperature(float inputTemperature){
+    temperature = inputTemperature;
+}
+
+void TemperatureSensor::setHumidity(float inputHumidity){
+    humidity = inputHumidity;
+}
+
+void TemperatureSensor::setThermalSensation(float inputThermalSensation){
+    thermalSensation = inputThermalSensation;
+}
+
+DHT TemperatureSensor::getDht(){
+    return dht;
+}
+
+String TemperatureSensor::getName(){
+    return name;
+}
+
+float TemperatureSensor::getPin(){
+    return pin;
+}
+
+float TemperatureSensor::getTemperature(){
+    return temperature;
+}
+
+float TemperatureSensor::getHumidity(){
+    return humidity;
+}
+
+float TemperatureSensor::getThermalSensation(){
+    return thermalSensation;
+}
+
+float TemperatureSensor::checkTemperature(){
+    temperature = dht.readTemperature();
+    return temperature;
+}
+
+float TemperatureSensor::checkHumidity(){
+    humidity = dht.readHumidity();
+    return humidity;
+}
+
+float TemperatureSensor::checkThermalSensation(){
+    float response = 0;
+
+    float t = checkTemperature();
+    float h = checkHumidity();
+
+    thermalSensation = dht.computeHeatIndex(t, h, false);
+
+    return thermalSensation;
+}
+
--- libraries/TemperatureSensor/TemperatureSensor.h
+++ libraries/TemperatureSensor/TemperatureSensor.h
@@ -2,13 +2,40 @@
 #define TemperatureSensor_h
 
 #include "Arduino.h"
+#include "DHT.h"
 
 class TemperatureSensor{
 
     private:
+        DHT dht;
+
+        String name;
+        float pin;
+        float temperature;
+        float humidity;
+        float thermalSensation;
 
     public:
         TemperatureSensor();
+        TemperatureSensor(String name, float pin);
+
+        void setDht(DHT dht);
+        void setName(String name);
+        void setPin(float pin);
+        void setTemperature(float temperature);
+        void setHumidity(float humidity);
+        void setThermalSensation(float thermalSensation);
+
+        DHT getDht();
+        String getName();
+        float getPin();
+        float getTemperature();
+        float getHumidity();
+        float getThermalSensation();
+
+        float checkTemperature();
+        float checkHumidity();
+        float checkThermalSensation();
     };
 
 #endif
