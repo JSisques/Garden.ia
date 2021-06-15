88adae4ec0287dc32f7debe51c96103f7c8ed3d3
Javi Plaza <javierplazasisques@gmail.com>
2021-06-15 18:34:10 +0200

    Updated Data library

--- libraries/Data/Data.cpp
+++ libraries/Data/Data.cpp
@@ -2,53 +2,17 @@
 #include "Data.h"
 
 Data::Data(){
-    thresholdTemperature = 20;
-    thresholdHumidity = 50;
-    thresholdWaterLevel = 20;
     thresholdSoilHumidity = 50;
 }
 
-Data::Data(String inputName, WaterPump wp, LightSensor ls, SoilSensor ss){
+Data::Data(String inputName, WaterPump wp, SoilSensor ss){
     name = inputName;
     waterPump = wp;
-    lightSensor = ls;
     soilSensor = ss;
-}
-
-void Data::setLight(int inputLight){
-    light = inputLight;
-}
-
-void Data::setTemperature(int inputTemperature){
-    temperature = inputTemperature;
-}
-
-void Data::setHumidity(int inputHumidity){
-    humidity = inputHumidity;
-}
-
-void Data::setWaterLevel(int inputWaterLevel){
-    waterLevel = inputWaterLevel;
 }
 
 void Data::setSoilHumidity(int inputSoilHumidity){
     soilHumidity = inputSoilHumidity;
-}
-
-void Data::setThresholdLight(int inputThresholdLight){
-    thresholdLight = inputThresholdLight;
-}
-
-void Data::setThresholdTemperature(int inputThresholdTemperature){
-    thresholdTemperature = inputThresholdTemperature;
-}
-
-void Data::setThresholdHumidity(int inputThresholdHumidity){
-    thresholdSoilHumidity = inputThresholdHumidity;
-}
-
-void Data::setThresholdWaterLevel(int inputThresholdWaterLevel){
-    thresholdWaterLevel = inputThresholdWaterLevel;
 }
 
 void Data::setThresholdSoilHumidity(int inputThresholdSoilHumidity){
@@ -59,40 +23,8 @@ void Data::setTrigger(bool inputTrigger){
     trigger = inputTrigger;
 }
 
-int Data::getLight(){
-    return light;    
-}
-
-int Data::getTemperature(){
-    return temperature;    
-}
-
-int Data::getHumidity(){
-    return humidity;
-}
-
-int Data::getWaterLevel(){
-    return waterLevel;
-}
-
 int Data::getSoilHumidity(){
     return soilHumidity;
-}
-
-int Data::getThresholdLight(){
-    return thresholdLight;
-}
-
-int Data::getThresholdTemperature(){
-    return thresholdTemperature;
-}
-
-int Data::getThresholdHumidity(){
-    return thresholdHumidity;
-}
-
-int Data::getThresholdWaterLevel(){
-    return thresholdWaterLevel;
 }
 
 int Data::getThresholdSoilHumidity(){
@@ -103,12 +35,11 @@ bool Data::getTrigger(){
     return trigger;
 }
 
-bool Data::checkNeedWater(){
+bool Data::checkNeedWater(int light, int thresholdLight){
 
     bool response = false;
 
     soilHumidity = soilSensor.getHumidity();
-    light = lightSensor.checkLight();
 
     if (soilHumidity <= thresholdSoilHumidity && light <= thresholdLight){
         response = true;
@@ -119,11 +50,19 @@ bool Data::checkNeedWater(){
 }
 
 void Data::resetData(){
-
-    temperature = 0;
-    humidity = 0;
-    waterLevel = 0;
     soilHumidity = 0;
     trigger = false;
 
 }
+
+int Data::checkSoilHumidity(){
+    soilHumidity = soilSensor.checkHumidity();
+    return soilHumidity;
+}
+
+void Data::water(bool trigger){
+    if(trigger){
+        waterPump.water(10);
+    }
+}
+
--- libraries/Data/Data.h
+++ libraries/Data/Data.h
@@ -10,58 +10,36 @@ class Data{
     private:
         String name;
 
-        int humidity;
-        int waterLevel;
         int soilHumidity;
         bool trigger;
 
-        int thresholdTemperature;
-        int thresholdHumidity;
-        int thresholdWaterLevel;
         int thresholdSoilHumidity;
 
         WaterPump waterPump;
-        LightSensor lightSensor;
         SoilSensor soilSensor;
 
     public:
         Data();
-        Data(String name, WaterPump wp, LightSensor ls, SoilSensor ss);
+        Data(String name, WaterPump wp, SoilSensor ss);
         
         void setName(String name);
-        void setLight(int light);
-        void setTemperature(int temperature);
-        void setHumidity(int humidity);
-        void setWaterLevel(int waterLevel);
         void setSoilHumidity(int soilHumidity);
-        void setThresholdLight(int thresholdLight);
-        void setThresholdTemperature(int thresholdTemperature);
-        void setThresholdHumidity(int thresholdHumidity);
-        void setThresholdWaterLevel(int thresholdWaterLevel);
         void setThresholdSoilHumidity(int thresholdSoilHumidity);
         void setTrigger(bool trigger);
         void setWaterPump(WaterPump waterPump);
-        void setLightSensor(LightSensor lightSensor);
         void setSoilSensor(SoilSensor soilSensor);
 
         String getName();
-        int getLight();
-        int getTemperature();
-        int getHumidity();
-        int getWaterLevel();
         int getSoilHumidity();
-        int getThresholdLight();
-        int getThresholdTemperature();
-        int getThresholdHumidity();
-        int getThresholdWaterLevel();
         int getThresholdSoilHumidity();
         bool getTrigger();
         WaterPump getWaterPump();
-        LightSensor getLightSensor();
         SoilSensor getSoilSensor();
 
-        bool checkNeedWater();
+        bool checkNeedWater(int light, int thresholdLight);
         void resetData();
+        int checkSoilHumidity();
+        void water(bool trigger);
     };
 
 #endif
