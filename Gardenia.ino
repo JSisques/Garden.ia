a431031d7a4568128446ca91f957d21966bfb559
Javi Plaza <javierplazasisques@gmail.com>
2021-06-15 18:41:29 +0200

    Updated main program

--- Gardenia.ino
+++ Gardenia.ino
@@ -2,24 +2,28 @@
 #include <WaterPump.h>
 #include <WaterLevel.h>
 #include <LightSensor.h>
+#include <TemperatureSensor.h>
+#include <AmbientData.h>
 #include <Data.h>
 
 PowerSave ps;
+
+TemperatureSensor ts("Temperature sensor", 10);
+LightSensor ls("Light sensor", 9);
+WaterLevel wl("Water level sensor", 10);
+AmbientData ad("Ambient data", ts, ls, wl);
 
 WaterPump wp1("Bomba 1", 11);
 WaterPump wp2("Bomba 2", 12);
 WaterPump wp3("Bomba 3", 13);
 
-WaterLevel wl("Water level sensor", 10);
-LightSensor ls("Light sensor", 9);
-
 SoilSensor ss1("Soil sensor 1", 10);
 SoilSensor ss2("Soil sensor 2", 10);
 SoilSensor ss3("Soil sensor 3", 10);
 
-Data d1("Data 1", wp1, ls, ss1);
-Data d2("Data 2", wp2, ls, ss2);
-Data d3("Data 3", wp3, ls, ss3);
+Data d1("Data 1", wp1, ss1);
+Data d2("Data 2", wp2, ss2);
+Data d3("Data 3", wp3, ss3);
 
 void setup() {
 }
@@ -27,28 +31,29 @@ void setup() {
 void loop() {
 
   //Check air temperature
+  ad.checkTemperature();
+
   //Check air humidity
-  //Check thermal temperature 
+  ad.checkHumidity();
+
+  //Check thermal sensation
+  ad.checkThermalSensation();
 
   //Check light
-  d1.setLight(ls.checkLight());
-  d2.setLight(ls.checkLight());
-  d3.setLight(ls.checkLight());
+  ad.checkLight();
+
+  //Check water level
+  ad.checkWaterLevel();
 
   //Check soil humidity
-  d1.setWaterLevel(d1.getSoilSensor().checkHumidity());
-  d2.setWaterLevel(d2.getSoilSensor().checkHumidity());
-  d3.setWaterLevel(d3.getSoilSensor().checkHumidity());
-  
-  //Check water level
-  d1.setWaterLevel(wl.checkWaterLevel());
-  d2.setWaterLevel(wl.checkWaterLevel());
-  d3.setWaterLevel(wl.checkWaterLevel());
+  d1.checkSoilHumidity();
+  d2.checkSoilHumidity();
+  d3.checkSoilHumidity();
 
   //Check if need water
-  d1.getWaterPump().water(d1.checkNeedWater());
-  d2.getWaterPump().water(d2.checkNeedWater());
-  d3.getWaterPump().water(d3.checkNeedWater());
+  d1.water(d1.checkNeedWater(ad.getLight(), ad.getThresholdLight()));
+  d2.water(d2.checkNeedWater(ad.getLight(), ad.getThresholdLight()));
+  d3.water(d3.checkNeedWater(ad.getLight(), ad.getThresholdLight()));
 
   //Sleep 1 hour
   ps.sleep(3600);
