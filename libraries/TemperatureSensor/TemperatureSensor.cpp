2642e7c3cd3534372783157be1e3ce96f2a99bf6
Javi Plaza <javierplazasisques@gmail.com>
2021-06-15 17:17:48 +0200

    Updated Temperature Sensor library

---
+++ libraries/TemperatureSensor/TemperatureSensor.cpp
@@ -0,0 +1,5 @@
+#include "Arduino.h"
+#include "TemperatureSensor.h"
+
+TemperatureSensor::TemperatureSensor(){
+}
---
+++ libraries/TemperatureSensor/TemperatureSensor.h
@@ -0,0 +1,14 @@
+#ifndef TemperatureSensor_h
+#define TemperatureSensor_h
+
+#include "Arduino.h"
+
+class TemperatureSensor{
+
+    private:
+
+    public:
+        TemperatureSensor();
+    };
+
+#endif
