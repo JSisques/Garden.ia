b58ae888c4f6879ab96586a5e0913e7682662f2b
Javi Plaza <javierplazasisques@gmail.com>
2021-06-15 17:51:32 +0200

    Created Ambient Data library

---
+++ libraries/AmbientData/AmbientData.h
@@ -0,0 +1,71 @@
+#ifndef Data_h
+#define Data_h
+
+#include "Arduino.h"
+#include "WaterPump.h"
+#include "LightSensor.h"
+#include "SoilSensor.h"
+
+class Data{
+
+    private:
+        String name;
+
+        int light;
+        int temperature;
+        int humidity;
+        int waterLevel;
+        int soilHumidity;
+        bool trigger;
+
+        int thresholdLight;
+        int thresholdTemperature;
+        int thresholdHumidity;
+        int thresholdWaterLevel;
+        int thresholdSoilHumidity;
+
+        WaterPump waterPump;
+        LightSensor lightSensor;
+        SoilSensor soilSensor;
+
+    public:
+        Data();
+        Data(String name, WaterPump wp, LightSensor ls, SoilSensor ss);
+        
+        void setName(String name);
+        void setLight(int light);
+        void setTemperature(int temperature);
+        void setHumidity(int humidity);
+        void setWaterLevel(int waterLevel);
+        void setSoilHumidity(int soilHumidity);
+        void setThresholdLight(int thresholdLight);
+        void setThresholdTemperature(int thresholdTemperature);
+        void setThresholdHumidity(int thresholdHumidity);
+        void setThresholdWaterLevel(int thresholdWaterLevel);
+        void setThresholdSoilHumidity(int thresholdSoilHumidity);
+        void setTrigger(bool trigger);
+        void setWaterPump(WaterPump waterPump);
+        void setLightSensor(LightSensor lightSensor);
+        void setSoilSensor(SoilSensor soilSensor);
+
+        String getName();
+        int getLight();
+        int getTemperature();
+        int getHumidity();
+        int getWaterLevel();
+        int getSoilHumidity();
+        int getThresholdLight();
+        int getThresholdTemperature();
+        int getThresholdHumidity();
+        int getThresholdWaterLevel();
+        int getThresholdSoilHumidity();
+        bool getTrigger();
+        WaterPump getWaterPump();
+        LightSensor getLightSensor();
+        SoilSensor getSoilSensor();
+
+        bool checkNeedWater();
+        void resetData();
+    };
+
+#endif
